35720730fbedff62e152e2da7175243df55e3f9d
Alexey Bachaev <bachaev2000@yandex.ru>
2024-04-15 20:51:58 +0300

    .

---
+++ naiveSearch.js
@@ -0,0 +1,13 @@
+//Поиск подстроки в строке
+
+function naiveSearch(mainStr, subStr) {
+  if (subStr.length > mainStr.length) return false;
+  for (let i = 0; i < mainStr.length; i++) {
+    for (let j = 0; j < subStr.length; j++) {
+      if (mainStr[i + j] !== subStr[j]) break;
+      if (j === subStr.length - 1) return true;
+    }
+  }
+
+  return false;
+}
